c72dddfd7ad73352fd7e25adc9bbfd5c6678ac2b
Evgeniy Shevchuk <EShevchuk@altareturn.com>
2020-04-08 22:07:50 -0700

    [master] 1 lesson

---
+++ lesson 1/index.js
@@ -0,0 +1,152 @@
+// let name = "Evgeniy";
+// let age_of_user = "Evgeniy";
+
+
+// let name;
+// // что-то происходит
+// name = "Oleg";
+// name = "Vitalii";
+//
+// const age = 18;
+// // age = 20;
+
+
+// const localConstant = 18;
+// const GLOBAL_CONSTANT = 20;
+//
+// if (2 > 1) {
+//     console.log(GLOBAL);
+// }
+
+// const nameOfUser = "Evgeniy Shevchuk";
+// const ageOfUser = 23;
+// const yearOfBirthOfUser = 1996;
+// const hasDriverLicense = true;
+// const isTaskComplete = false;
+// const currentHomework = null;
+
+// undefined
+// let variable;
+// console.log(variable);
+// variable = "string varibale";
+// console.log(variable);
+
+// let variable = 123;
+// console.log(variable);
+// variable = undefined;
+// console.log(variable);
+
+// let variable = true;
+// variable = "one";
+
+// console.log(variable);
+// console.log(typeof variable);
+
+
+
+// let decladerVariable;
+// console.log(decladerVariable);
+// console.log(undecladerVariable);
+
+// console.log("hello", "world", "how", "are", "you", 123, undefined);
+// alert("Hello world");
+// const userDecision = confirm("hello world");
+// console.log(userDecision);
+// const userInput = prompt("how old are you?");
+// alert(userInput);
+
+// let result = 10 + 20;
+// console.log(result);
+//
+// result = 10 - 5;
+// console.log(result);
+//
+// result = 10 - 20;
+// console.log(result);
+//
+// result = 2 * 4;
+// console.log(result);
+//
+// result = 10 / 5;
+// console.log(result);
+//
+// result = 10 / 4;
+// console.log(result);
+//
+// result = 10 % 4;
+// console.log(result);
+
+// result = 10 % 3;
+// console.log(result);
+
+//
+// let value = 10;
+// // value = value + 5;
+// value *= 5;
+// console.log(value);
+
+
+// const x = 5;
+// const y = 6;
+// const result = x < y;
+// console.log(result);
+//
+// console.log(7 >= 8);
+
+// console.log(6 === 6)
+// console.log(5 === 6);
+// console.log(5 !== 6);
+// console.log(6 !== 6);
+
+// console.log(1 === "1");
+
+// let number = 123;
+// number += 5;
+// number -= 100;
+// console.log(number);
+
+const ageOfUser = prompt("how old are you?");
+// const numericValue = Number(ageOfUser);
+const numericValue = Number.parseFloat(ageOfUser);
+console.log(numericValue, typeof numericValue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
